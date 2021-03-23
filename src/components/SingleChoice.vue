1f1c423b7da27ef99dbe13bcba0f178298283abe
Megrax <geronimooo@foxmail.com>
2021-03-23 22:20:25 +0800

    complete: SingleChoiceDemo & realize: csr

--- src/components/SingleChoice.vue
+++ src/components/SingleChoice.vue
@@ -1,33 +1,48 @@
 <template>
-  <main class="flex flex-col flex-nowrap justify-center items-center h-96">
-    <ul class="flex flex-row flex-nowrap justify-center items-start w-11/12 min-h-full h-80 mt-24">
-      <li class="flex flex-col flex-nowrap items-center justify-around w-48 h-42 pt-4 pb-4 pl-3 pr-3">
-        <div>{{ practiceTitle }}</div>
-        <div>{{ practiceTime }}</div>
-        <div>{{ practiceFullMarks }}</div>
-      </li>
-      <li class="flex flex-col flex-nowrap justify-around items-center w-5/6 min-h-full ml-32 mr-32">
-        <div class="pl-6 pr-5 pt-4">{{ questions[currIndex].question }}</div>
-        <ul class="flex flex-col justify-around pt-5 pb-5">
-          <li v-for="option in questions[currIndex].options" :key="option.id" class="flex flex-row items-center justify-start w-96 pt-2">
-            <input type="radio" :value="option.id" v-model="inputAnswers[currIndex].answer" id="radio">
-            <span class="ml-5">{{ option.value }}</span>
-          </li>
-        </ul>
-      </li>
-      <li class="flex flex-col flex-nowrap items-center justify-center w-48 h-36">
-        <p>{{ countdown }}</p>
-        <Countdown time="3600" step="1" format="hh:mm:ss" class="text-2xl mt-4">
-          <template slot-scope="{ time }">{{ time }}</template>
-        </Countdown>
-      </li>
+  <div>
+    <main class="flex flex-col flex-nowrap justify-center items-center h-96">
+      <ul class="flex flex-row flex-nowrap justify-center items-start w-11/12 min-h-full h-80 mt-48">
+        <li class="flex flex-col flex-nowrap items-center justify-around w-48 h-42 pt-4 pb-4 pl-3 pr-3 border shadow-md">
+          <div>{{ practiceTitle }}</div>
+          <div>{{ practiceTime }}</div>
+          <div>{{ practiceFullMarks }}</div>
+        </li>
+        <li class="flex flex-col flex-nowrap justify-around items-center w-5/6 min-h-full ml-32 mr-32 border shadow-md">
+          <div class="pl-6 pr-5 pt-4">
+            <span>{{ questions[currIndex].id }}</span>
+            <i>.</i>
+            <span>{{ questions[currIndex].question }}</span>
+          </div>
+          <ul class="flex flex-col justify-around pt-5 pb-5">
+            <li v-for="option in questions[currIndex].options" :key="option.id" class="flex flex-row items-center justify-start w-96 pt-2">
+              <label class="cursor-pointer">
+                <input type="radio" :value="option.id" v-model="userAnswers[currIndex].answer" id="radio">
+                <span class="ml-5">
+                  <span>{{ option.id }}</span>
+                  <i>.</i>
+                  <span class="ml-3">{{ option.value }}</span>
+                </span>
+              </label>
+            </li>
+          </ul>
+        </li>
+        <li class="flex flex-col flex-nowrap items-center justify-center w-48 h-36">
+          <p>{{ countdown }}</p>
+          <Countdown :time="3600" :step="1" format="hh:mm:ss" class="text-2xl mt-4">
+            <template slot-scope="{ time }">{{ time }}</template>
+          </Countdown>
+        </li>
+      </ul>
+      <div class="flex flex-row flex-nowrap justify-around items-center w-11/12 h-24 mt-8">
+        <button class="flex items-center justify-center w-32 h-10 bg-startPractice text-white" :class="{'btn-disabled': (prevDisabled || currIndex === 0)}" @click="handlePrevClick">{{ prevQ }}</button>
+        <button class="flex items-center justify-center w-32 h-10 bg-startPractice text-white" :class="{'btn-disabled': (nextDisabled || currIndex === questions.length - 1 )}" @click="handleNextClick">{{ nextQ }}</button>
+        <button class="flex items-center justify-center w-32 h-10 bg-startPractice text-white" :class="{'btn-disabled': currIndex !== questions.length - 1 }" @click="handleSubmit">{{ submit }}</button>
+      </div>
+    </main>
+    <ul class="flex flex-row flex-nowrap gap-x-2 justify-center items-center w-screen mt-52">
+      <li @click="handleTabClick(question.id)" v-for="question in questions" :key="question.id" class="flex justify-center items-center w-8 h-8 bg-gray-200 rounded-full cursor-pointer" :class="{ 'btn-selected': (parseInt(question.id) === currIndex + 1) }">{{ question.id }}</li>
     </ul>
-    <div class="flex flex-row flex-nowrap justify-around items-center w-11/12 h-24 mt-8">
-      <button class="flex items-center justify-center w-32 h-10 bg-startPractice text-white" :class="{'btn-disabled': (prevDisabled || currIndex === 0)}" @click="handlePrevClick">{{ prevQ }}</button>
-      <button class="flex items-center justify-center w-32 h-10 bg-startPractice text-white" :class="{'btn-disabled': (nextDisabled || currIndex === questions.length - 1 )}" @click="handleNextClick">{{ nextQ }}</button>
-      <button class="flex items-center justify-center w-32 h-10 bg-startPractice text-white" :class="{'btn-disabled': currIndex !== questions.length - 1 }" @click="handleSubmit">{{ submit }}</button>
-    </div>
-  </main>
+  </div>
 </template>
 
 <script>
@@ -38,28 +53,7 @@ export default {
     return {
       /* 逻辑控制 */
       currIndex: 0,
-      inputAnswers: [
-        {
-          id: 1,
-          answer: ''
-        },
-        {
-          id: 2,
-          answer: ''
-        },
-        {
-          id: 3,
-          answer: ''
-        },
-        {
-          id: 4,
-          answer: ''
-        },
-        {
-          id: 5,
-          answer: ''
-        },
-      ],
+      userAnswers: [],
       /* 基础构件 */
       prevQ: '上一题',
       nextQ: '下一题',
@@ -72,79 +66,22 @@ export default {
       practiceFullMarks: '100分',
       countdown: '剩余时间：',
       /* 题目信息 */
-      questions: [
-        {
-          id: '1',
-          name: 'one',
-          type: '单选',
-          question:
-            '在 css 选择器当中，优先级排序正确的是：',
-          options: [
-            { id: 'A', value: 'id选择器>标签选择器>类选择器' },
-            { id: 'B', value: '标签选择器>类选择器>id选择器' },
-            { id: 'C', value: '类选择器>标签选择器>id选择器' },
-            { id: 'D', value: 'id选择器>类选择器>标签选择器' },
-          ],
-          answer: 'D'
-        },
-        {
-          id: '2',
-          name: 'two',
-          type: '单选',
-          question:
-            '关于HTML语义化，以下哪个说法是正确的？',
-          options: [
-            { id: 'A', value: '语义化的HTML有利于机器的阅读，如PDA手持设备、搜索引擎爬虫；但不利于人的阅读' },
-            { id: 'B', value: 'Table 属于过时的标签，遇到数据列表时，需尽量使用 div 来模拟表格' },
-            { id: 'C', value: '语义化是HTML5带来的新概念，此前版本的HTML无法做到语义化' },
-            { id: 'D', value: 'header、article、address都属于语义化明确的标签' },
-          ],
-          answer: 'D',
-        },
-        {
-          id: '3',
-          name: 'three',
-          type: '单选',
-          question:
-            '放在HTML里的哪一部分JavaScripts会在页面加载的时候被执行？',
-          options: [
-            { id: 'A', value: '文件头部位置' },
-            { id: 'B', value: '文件尾' },
-            { id: 'C', value: '<head>标签部分' },
-            { id: 'D', value: '<body>标签部分' },
-          ],
-          answer: 'D',
-        },
-        {
-          id: '4',
-          name: 'four',
-          type: '单选',
-          question:
-            'JavaScript 的基本组成不包括以下哪个选项',
-          options: [
-            { id: 'A', value: 'DOM' },
-            { id: 'B', value: 'BOM' },
-            { id: 'C', value: 'ECMAScript' },
-            { id: 'D', value: 'jQuery' },
-          ],
-          answer: 'D',
-        },
-        {
-          id: '5',
-          name: 'five',
-          type: '单选',
-          question:
-            '在javascript里，下列选项中不属于数组方法的是：',
-          options: [
-            { id: 'A', value: 'sort()' },
-            { id: 'B', value: 'length()' },
-            { id: 'C', value: 'concat()' },
-            { id: 'D', value: 'reverse()' },
-          ],
-          answer: 'B',
-        },
-      ]
+      questions: []
     }
+  },
+  mounted() {
+    const url = 'http://localhost:3000/crowds'
+    fetch(url)
+      .then((res) => {
+        return res.json()
+      })
+      .then((json) => {
+        this.questions = json
+        this.generateUserAnswers()
+        console.log(this.questions)
+        console.log(this.userAnswers)
+      })
+
   },
   methods: {
     handlePrevClick() {
@@ -157,7 +94,7 @@ export default {
       }
     },
     handleNextClick() {
-      if (this.currIndex + 1 < this.questions.length) {
+      if (this.currIndex < this.questions.length - 1) {
         this.currIndex++
         this.nextDisabled = false
         this.prevDisabled = false
@@ -166,12 +103,31 @@ export default {
       }
     },
     handleSubmit() {
-      let result = []
-      for (const answer of this.inputAnswers) {
-        result.push(answer.answer)
+      if (this.currIndex === this.questions.length - 1) {
+        let result = []
+        for (const answer of this.userAnswers) {
+          result.push(answer.answer)
+        }
+        let msg = `你提交的答案是：${result}`
+        alert(msg)
       }
-      let msg = `你提交的答案是：${result}`
-      alert(msg)
+    },
+    handleTabClick(id) {
+      this.currIndex = id - 1
+    },
+    generateUserAnswers() {
+      let obj = {
+        id: 0,
+        answer: ''
+      }
+      for (const item of this.questions) {
+        obj = {
+          id: 0,
+          answer: ''
+        }
+        obj.id = item.id
+        this.userAnswers.push(obj)
+      }
     }
   },
   components: {
